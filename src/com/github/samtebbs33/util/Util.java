23f30a408ac4a472124b6281475887dc0fc92128
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-03-17 14:41:01 +0000

    Added Util.java and Util.array()

---
+++ src/com/github/samtebbs33/util/Util.java
@@ -0,0 +1,12 @@
+public class Util {
+
+    /**
+    * Returns the var-args as an array. Useful for methods that require an array but the conciseness of var-args is desired.
+    * @param T... objs - the var-args to return
+    * @return T[] - the var-args
+    */
+    public static <T> T[] array(T... objs) {
+    	return objs;
+    }
+
+}
